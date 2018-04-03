042237a934222b06f057c6f6f9bcec8feb0ccac8
ING <jcfiguero@miuandes.cl>
2018-04-03 15:53:26 -0300

    First commit for april 3 lab

---
+++ ConsoleApp1/ConsoleApp1/vehiculo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class vehiculo
+    {
+        public string Auto, Acuatico, Moto, Camion, Bus, MaquinariaP;
+        public vehiculo(string auto, string acuatico, string moto, string camion, string bus , string maquinariap)
+        {
+            this.Auto = auto;
+            this.Acuatico = acuatico;
+            this.Moto = moto;
+            this.Camion = camion;
+            this.Bus= bus:
+            this.MaquinariaP=maquinariap
+        }
+          
+    }
+}
